5a0507932ef33db2470d49559ee3521c35e93f69
Gui Wuff <gui.wuff@gmail.com>
2019-05-04 00:29:29 +0700

    Add sprite dev in 0.1.3

--- dist/collection/globals/index.js
+++ dist/collection/globals/index.js
@@ -1,2 +1,2 @@
 import { spriteInject } from 'icon-sprites-inject';
-spriteInject(`https://cdn.jsdelivr.net/gh/guiwuff/icon-sprites-inject@latest/dist/assets/sprites.svg`, {});
+spriteInject(`https://cdn.jsdelivr.net/gh/guiwuff/icon-sprites-inject@latest/dist/assets/sprites-dev.svg`, {});
--- src/globals/index.ts
+++ src/globals/index.ts
@@ -1,3 +1,3 @@
 import { spriteInject } from 'icon-sprites-inject';
 
-spriteInject(`https://cdn.jsdelivr.net/gh/guiwuff/icon-sprites-inject@latest/dist/assets/sprites.svg`, {});
+spriteInject(`https://cdn.jsdelivr.net/gh/guiwuff/icon-sprites-inject@latest/dist/assets/sprites-dev.svg`, {});
